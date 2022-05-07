6a459fb8e8817146202eee6338f39519d168410c
Sender Flores <safcrace@gmail.com>
2022-05-06 21:16:24 -0600

    API Error Handling Implementation

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _dbContext;
+
+        public BuggyController(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("not_found")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _dbContext.Products.Find(42);
+            if (thing is null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("server_error")]
+        public ActionResult GetServerError()
+        {
+            var thing = _dbContext.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("bad_request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("bad_request/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+
+        }
+    }
+}
